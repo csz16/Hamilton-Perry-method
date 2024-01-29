83bf3e34308dc00c39b56a4e77044b90a2a3c7c1
SIZHE CHEN <61646682+csz16@users.noreply.github.com>
2024-01-29 16:32:54 +1100

    Add files via upload

--- Initial setting.R
+++ Initial setting.R
@@ -1,31 +1,133 @@
-#Load package
-library("ggplot2")
-library("MASS")
-library("tidyverse")
-library("tibble")
-library("readr")
-library("MASS")
-library("tidyr")
-library("plyr")
-library("dplyr")
-library("ftsa")
-library("reshape2")
-library("R2jags")
+require(demography)
+require(ftsa)
+require(TSA)
+
+####################################
+# Function to read demographic data
+####################################
+
+read_demographic_data <- function(state, dataType, type){
+  valid_States <- c("ACT", "NSW", "QLD", "WA", "NT", "VIC", "TAS", "SA")
+  stopifnot(state %in% valid_States)
+  
+  # valid_dataType <- c("Births", "Deaths_1x1", "Population", "Exposures_1x1", "Mx_1x1",  "fltper_1x1", "mltper_1x1", "bltper_1x1", "E0per_1x1")
+  # stopifnot(dataType %in% valid_dataType)
+  
+  baseUrl <- "https://aushd.org/assets/txtFiles/humanMortality"
+  url <- paste(baseUrl, state, dataType, sep = "/")  # Construct the URL
+  
+  # Attempt to read data
+  tryCatch({
+    data <- read.table(url(url), skip = 2, , na.strings = ".", header = TRUE)
+    return(data)
+  }, error = function(e) {
+    cat("Error in reading data:", e$message, "\n")
+    return(NULL)
+  })
+}
+
+valid_States <- c("ACT", "NSW", "QLD", "WA", "NT", "VIC", "TAS", "SA")
+# valid_dataType <- c("Births", "Deaths_1x1", "Population.txt", "Exposures_1x1", "Mx_1x1")
+
+results <- list()
+dataType <- "Population.txt"
+type <- "Population"
 
 
+# Nested loop to read all data combinations
+for (state in valid_States){
+  # Read data and store in results list
+  results[[paste(state, type, sep = "_")]] <- read_demographic_data(state, dataType, type)
+}
 
-#Read data
-data <- read.table("Population_data.txt",header = T)
-pop_m <- readRDS("pop_m.rds")
-pop_f <- readRDS("pop_f.rds")
+female_matrices <- list()
+male_matrices <- list()
 
+# Number of rows for each matrix (assuming 111 as in your example)
+n_rows <- 111
 
+# Loop through each data frame in the list
+for (state in names(results)) {
+  # Extract the female and male data and convert them into matrices
+  female_matrices[[state]] <- matrix(results[[state]][,3], n_rows)
+  male_matrices[[state]] <- matrix(results[[state]][,4], n_rows)
+}
 
-#initial value
-Age <- unique(data$Age)
-Year <- unique(data$Year)
-n <- length(Age)
-m <- length(Year)
-m.ratio <- 105.7/(105.7+100) 
-f.ratio <- 1-m.ratio
+level =0.95
 method <- "classical"
+year = 1971:2021
+n_year = length(year)
+age = 0:100
+n_age = length(age)
+
+for (i in 1:8) {
+  female_matrices[[i]][101,] <- colSums(female_matrices[[i]][101:111,])
+  female_matrices[[i]] <- female_matrices[[i]][-c(102:111),]
+  male_matrices[[i]][101,] <- colSums(male_matrices[[i]][101:111,])
+  male_matrices[[i]] <- male_matrices[[i]][-c(102:111),]
+  rownames(female_matrices[[i]]) = rownames(male_matrices[[i]]) = age
+  colnames(female_matrices[[i]]) = colnames(male_matrices[[i]]) = year
+}
+
+
+au_pop_f <- matrix(0,111,51)
+au_pop_m <- matrix(0,111,51)
+
+for (i in 1:8) {
+  au_pop_f <- au_pop_f + female_matrices[[i]]
+  au_pop_m <- au_pop_m + male_matrices[[i]]
+}
+
+au_pop_f[101,] <- colSums(au_pop_f[101:111,])
+au_pop_f <- au_pop_f[-c(102:111),]
+au_pop_m[101,] <- colSums(au_pop_m[101:111,])
+au_pop_m <- au_pop_m[-c(102:111),]
+rownames(au_pop_f) = rownames(au_pop_m) = age
+colnames(au_pop_f) = colnames(au_pop_m) = year
+
+
+newborn <- read.csv("ABS_BIRTHS_SUMMARY.csv",header = T)
+
+newborn_2022 <- newborn %>% dplyr:::filter(newborn$TIME_PERIOD..Time.Period==2022)
+newborn_2022 <- newborn_2022[,c(2,3,5,6)]
+colnames(newborn_2022) <- c("measure","region","time_period","value")
+region_map <- list(
+  "1: New South Wales" = "NSW",
+  "2: Victoria" = "VIC",
+  "3: Queensland" = "QLD",
+  "4: South Australia" = "SA",
+  "5: Western Australia" = "WA",
+  "6: Tasmania" = "TAS",
+  "7: Northern Territory" = "NT",
+  "8: Australian Capital Territory" = "ACT",
+  "AUS: Australia" = "AUS"
+)
+
+# Replace full names with abbreviations
+newborn_2022$region <- sapply(newborn_2022$region, function(x) region_map[[x]])
+
+
+# Filter the dataset for the valid states and required measures
+filtered_data <- newborn_2022 %>%
+  dplyr:::filter(region %in% valid_states, measure %in% c("1: Births", "4: Male births"))
+
+
+# Calculate the male ratio for each state
+sex_ratio <- filtered_data %>%
+  group_by(region) %>%
+  summarise(
+    Total_Births = sum(value[measure == "1: Births"]),
+    Male_Births = sum(value[measure == "4: Male births"]),
+    Male_Ratio = Male_Births / Total_Births
+  )
+
+sex_ratio$Female_Ratio <- 1-sex_ratio$Male_Ratio
+sex_ratio <- as.data.frame(sex_ratio)
+
+# Convert the region column to a factor with levels in the desired order
+sex_ratio$region <- factor(sex_ratio$region, levels = valid_states)
+
+# Arrange the dataframe based on the ordered factor
+sex_ratio <- dplyr::arrange(sex_ratio, region)
+
+
--- Population forecast via H-P method.R
+++ Population forecast via H-P method.R
@@ -1,164 +1,245 @@
-population.fore <- function(pop_f,pop_m,method,N,f.ratio,m.ratio,level){
+linear_interpolated_pop <- function(data, new_age, age, forecast_year) {
+  # Initialize an empty list to store interpolated data for each year
+  interpolated_data_list <- list()
+  
+  # Iterate over each year (column in the matrix)
+  for (year_index in 1:ncol(data)) {
+    year_data <- data[, year_index]
+    interpolated_population <- approx(x = age, y = year_data, xout = new_age)$y
+    interpolated_data_list[[year_index]] <- interpolated_population
+  }
+  
+  # Convert the list to a matrix
+  # Each column in this new matrix will correspond to a year, as in the original matrix
+  interpolated_matrix <- do.call(cbind, interpolated_data_list)
+  if (ncol(interpolated_matrix)==length(forecast_year)) {
+    colnames(interpolated_matrix) <- forecast_year
+  }
+  rownames(interpolated_matrix) <- new_age
+  return(interpolated_matrix)
+}
+
+
+pension_age_interpolated <- function(data)
+{
+  pop_total_1 <- data[c(which(rownames(data)==16):nrow(data)),]
+  n2 <- nrow(pop_total_1)-1
+  m2 <- ncol(pop_total_1)
+  OADR <- matrix(data = 0, nrow = n2, ncol = m2, dimnames = list(rownames(pop_total_1[-1,]),colnames(pop_total_1)))
+  
+  for(j in 1:m2){
+    for(i in 1:n2) 
+    {
+      OADR[i,j] <- sum(pop_total_1[c((i+1):(n2+1)),j])/sum(pop_total_1[c(1:i),j])
+    }
+  }
+  
+  result <- apply(OADR, 2, function(x) min(which(x < 0.23)))
+  pension_age <- row.names(pop_total_1[result,])
+  final_result <- cbind(colnames(pop_total_1), pension_age)
+  colnames(final_result) <- c("year", "pension.age")
+  final_result <- as.data.frame(final_result)
+  final_result[] <- lapply(final_result, as.numeric)
+  result_list <- list(final_result = final_result, OADR = OADR)
+  return(result_list)
+}
+
+
+
+pension_age_bound_interpolated <- function(data, alpha)
+{
+  pop_total_1 <- data[c(which(rownames(data)==16):nrow(data)),]
+  n2 <- nrow(pop_total_1)-1
+  m2 <- ncol(pop_total_1)
+  OADR <- matrix(data = 0,nrow = n2,ncol = m2)
+  rownames(OADR) <- rownames(data[-1])
+  
+  for(j in 1:m2) 
+  {
+    for(i in 1:n2) 
+    {
+      OADR[i,j] <- sum(pop_total_1[c((i+1):(n2+1)),j])/sum(pop_total_1[c(1:i),j])
+    }
+  }
+  
+  result <- apply(OADR, 2, function(x) min(which(x < 0.23)))
+  pension_age <- as.numeric(row.names(pop_total_1[result,]))
+  final_result <- quantile(pension_age, alpha)
+  return(final_result)
+}
+
+population_fore_interpolated <- function(pop_f, pop_m, method, N, f.ratio, m.ratio, level, year, age)
+{
   # intial value
   alpha <- 1-level
-  
   pop_f.u <- pop_f
   pop_m.u <- pop_m
   pop_f.l <- pop_f
   pop_m.l <- pop_m
-  orginal_f <- pop_f
-  orginal_m <- pop_m
+  original_f <- pop_f
+  original_m <- pop_m
+  last_year <- year[length(year)]
+  forecast_year <- c((last_year+1):(last_year+N))
+  fore_boost_f <- list()
+  fore_boost_m <- list()
   
-  for (k in 1:N) { 
+  for(k in 1:N) 
+  {   
+    if(any(original_f == 0))
+    {
+      # Add 0.01 to all data points
+      pop_f <- pop_f + 0.01
+    } 
+    
+    if(any(original_m == 0))
+    {
+      # Add 0.01 to all data points
+      pop_m <- pop_m + 0.01
+    } 
     #one-step-ahead forecast
     n1 <- nrow(pop_f)-1
     m1 <- ncol(pop_f)-1
     
     # Compute the Cohort Component Ratio (CCR) for males
-    CCR_m <- matrix(data = 0,nrow = n1,ncol = m1,dimnames = list(rownames(pop_f[-1,]),colnames(pop_m)[-1]))
-    for (i in 1:n1) {
-      for (j in 1:m1) {
+    CCR_m <- matrix(data = 0, nrow = n1, ncol = m1, dimnames = list(rownames(pop_f[-1,]),colnames(pop_m)[-1]))
+    for(i in 1:n1) 
+    {
+      for(j in 1:m1) 
+      {
         CCR_m[i,j] <- pop_m[i+1,j+1]/pop_m[i,j]
       }
     }
     
     # Compute the CCR for females
     CCR_f <- matrix(data = 0,nrow = n1,ncol = m1,dimnames = list(rownames(pop_m[-1,]),colnames(pop_f)[-1]))
-    for (i in 1:n1) {
-      for (j in 1:m1) {
+    for(i in 1:n1) 
+    {
+      for(j in 1:m1) 
+      {
         CCR_f[i,j] <- pop_f[i+1,j+1]/pop_f[i,j]
       }
-    } 
+    }
     
     # Calculate the Child-Woman Ratio (CWR)
-    child <- colSums(pop_f[1,])+colSums(pop_m[1,])
+    child <- pop_f[1,]+pop_m[1,]
     women <- as.numeric(colSums(pop_f[c(16:50),]))
     
-    # Forecast the CWR    
-    CWR.m <- forecast(auto.arima(as.numeric(child/women)), h = 1, level = level)
-    CWR <- as.numeric(CWR.m$mean)
-    
-    # Obtain upper and lower bound of the forecast
-    CWR.u <- as.numeric(CWR.m$upper)
-    CWR.l <- as.numeric(CWR.m$lower)
+    # Bootstrap forecast the CWR    
+    CWR_bootstrap <- replicate(100, simulate(auto.arima(as.numeric(child/women)), nsim = 1))
     
     # Forecast the age 0-1 female and male populations
-    fore.pop.f <- women[length(women)]*CWR*f.ratio
-    fore.pop.m <- women[length(women)]*CWR*m.ratio
+    fore.pop.f <- women[length(women)]*mean(CWR_bootstrap)*f.ratio
+    fore.pop.m <- women[length(women)]*mean(CWR_bootstrap)*m.ratio
     
-    # Forecast the upper and lower bounds for age 0-1 female and male populations
-    fore.pop.f.u <- women[length(women)]*CWR.u*f.ratio
-    fore.pop.f.l <- women[length(women)]*CWR.l*f.ratio
+    m1.1 <- forecast.ftsm(ftsm(fts(0:(nrow(pop_m)-2),CCR_m), order = 6, method = method, weight = FALSE), h = 1,level = level, pimethod = "nonparametric")
     
-    fore.pop.m.u <- women[length(women)]*CWR.u*m.ratio
-    fore.pop.m.l <- women[length(women)]*CWR.l*m.ratio
+    fore.m <- c(fore.pop.m, m1.1$mean$y*pop_m[-nrow(pop_m),ncol(pop_m)])
+    fore_boost_m[[k]] <- rbind(women[length(women)]*CWR_bootstrap*m.ratio, m1.1$bootsamp[1:nrow(CCR_m), ,1]*pop_m[-nrow(pop_m),ncol(pop_m)])
     
-    m1.1 <- fts(0:99,CCR_m)
-    m1.2 <- ftsm(y = m1.1, order = 6, method = method, weight = FALSE)
-    m1.3 <- forecast.ftsm(object = m1.2,h = 1,level = level, pimethod = "nonparametric", seed = 10)
-    
-    # Forecast the female and male populations
-    fore.m <- c(fore.pop.m,m1.3$mean$y*pop_m[-nrow(pop_m),ncol(pop_m)])
+    f1.1 <- forecast.ftsm(ftsm(fts(0:(nrow(pop_f)-2),CCR_f), order = 6, method = method, weight = FALSE), h = 1,level = level, pimethod = "nonparametric")
     
     # Forecast the upper and lower bounds for female and male populations
-    upper.m <- apply(m1.3$bootsamp, 1, function(x) quantile(x,1-alpha/2))
-    lower.m <- apply(m1.3$bootsamp, 1, function(x) quantile(x,alpha/2))
+    fore.f <- c(fore.pop.f, f1.1$mean$y*pop_f[-nrow(pop_f),ncol(pop_f)])
+    fore_boost_f[[k]] <- rbind(women[length(women)]*CWR_bootstrap*f.ratio, f1.1$bootsamp[1:nrow(CCR_f), ,1]*pop_f[-nrow(pop_f),ncol(pop_f)])
     
-    fore.m.u <- c(fore.pop.m.u,upper.m*pop_m.u[-nrow(pop_m.u),ncol(pop_m.u)])
-    fore.m.l <- c(fore.pop.m.l,lower.m*pop_m.l[-nrow(pop_m.l),ncol(pop_m.l)])
+    #Combine the data set (Hanlin suggests below should be removed)
     
-    f1.1 <- fts(0:99,CCR_f)
-    f1.2 <- ftsm(y = f1.1, order = 6, method = method, weight = FALSE)
-    f1.3 <- forecast.ftsm(object = f1.2,h = 1,level = level, pimethod = "nonparametric", seed = 10)
+    pop_f <- cbind(pop_f, fore.f)
+    pop_m <- cbind(pop_m, fore.m)
     
-    fore.f <- c(fore.pop.f,f1.3$mean$y*pop_f[-nrow(pop_f),ncol(pop_f)])
-    upper.l <- apply(f1.3$bootsamp, 1, function(x) quantile(x,1-alpha/2))
-    lower.l <- apply(f1.3$bootsamp, 1, function(x) quantile(x,alpha/2))
+    pop_f.u <- cbind(pop_f.u, apply(fore_boost_f[[k]], 1, function(x) quantile(x,1-alpha/2)))
+    pop_m.u <- cbind(pop_m.u, apply(fore_boost_m[[k]], 1, function(x) quantile(x,1-alpha/2)))
     
-    fore.f.u <- c(fore.pop.f.u,upper.l*pop_f.u[-nrow(pop_f.u),ncol(pop_f.u)])
+    pop_f.l <- cbind(pop_f.l, apply(fore_boost_f[[k]], 1, function(x) quantile(x,alpha/2)))
+    pop_m.l <- cbind(pop_m.l, apply(fore_boost_m[[k]], 1, function(x) quantile(x,alpha/2)))
     
-    fore.f.l <- c(fore.pop.f.l,lower.l*pop_f.l[-nrow(pop_f.l),ncol(pop_f.l)])
+    colnames(pop_f)[ncol(pop_f)] <- as.character(as.numeric(colnames(pop_f)[ncol(pop_f)-1])+1)
+    colnames(pop_m)[ncol(pop_m)] <- as.character(as.numeric(colnames(pop_m)[ncol(pop_m)-1])+1)
     
-    #Combine the data set
-    pop_f <- cbind(pop_f,fore.f)
-    pop_m <- cbind(pop_m,fore.m)
+    colnames(pop_f.u)[ncol(pop_f.u)] <- as.character(as.numeric(colnames(pop_f)[ncol(pop_f)-1])+1)
+    colnames(pop_m.u)[ncol(pop_m.u)] <- as.character(as.numeric(colnames(pop_m.u)[ncol(pop_m.u)-1])+1)
     
-    pop_f.u <- cbind(pop_f.u,fore.f.u)
-    pop_m.u <- cbind(pop_m.u,fore.m.u)
+    colnames(pop_f.l)[ncol(pop_f.l)] <- as.character(as.numeric(colnames(pop_f.l)[ncol(pop_f.l)-1])+1)
+    colnames(pop_m.l)[ncol(pop_m.l)] <- as.character(as.numeric(colnames(pop_m.l)[ncol(pop_m.l)-1])+1)
     
-    pop_f.l <- cbind(pop_f.l,fore.f.l)
-    pop_m.l <- cbind(pop_m.l,fore.m.l)
+    if(any(original_f == 0))
+    {
+      # Subtract 0.01 to all data points
+      pop_f <- pop_f - 0.01
+      pop_f.u <- pop_f.u - 0.01
+      pop_f.l <- pop_f.l - 0.01
+    } 
     
-    names(pop_f)[ncol(pop_f)] <- as.character(as.numeric(colnames(pop_f[ncol(pop_f)-1]))+1)
-    names(pop_m)[ncol(pop_m)] <- as.character(as.numeric(colnames(pop_m[ncol(pop_m)-1]))+1)
-    
-    names(pop_f.u)[ncol(pop_f.u)] <- as.character(as.numeric(colnames(pop_f[ncol(pop_f)-1]))+1)
-    names(pop_m.u)[ncol(pop_m.u)] <- as.character(as.numeric(colnames(pop_m.u[ncol(pop_m.u)-1]))+1)
-    
-    names(pop_f.l)[ncol(pop_f.l)] <- as.character(as.numeric(colnames(pop_f.l[ncol(pop_f.l)-1]))+1)
-    names(pop_m.l)[ncol(pop_m.l)] <- as.character(as.numeric(colnames(pop_m.l[ncol(pop_m.l)-1]))+1)
+    if(any(original_m == 0))
+    {
+      # Subtract 0.01 to all data points
+      pop_m <- pop_m - 0.01
+      pop_m.u <- pop_m.u - 0.01
+      pop_m.l <- pop_m.l - 0.01
+    } 
+    print(k); rm(k)
   }
   
-  pop.t <- pop_f+pop_m
-  pop.t.1 <- pop.t[c(16:101),c(102:ncol(pop.t))]
-  n2 <- nrow(pop.t.1)-1
-  m2 <- ncol(pop.t.1)
-  OADR <- matrix(data = 0,nrow = n2,ncol = m2,dimnames = list(rownames(pop.t.1[-1,]),colnames(pop.t.1)))
+  new_age <- seq(0, 100, by = 0.1)
   
-  for (j in 1:m2) {
-    for (i in 1:n2) {
-      OADR[i,j] <- sum(pop.t.1[c((i+1):(n2+1)),j])/sum(pop.t.1[c(1:i),j])
-    }
+  fore_pop_total_mean <- pop_m[,which(colnames(pop_m)%in%forecast_year)] + pop_f[,which(colnames(pop_f)%in%forecast_year)]
+  
+  interpolated_fore_total_mean <- linear_interpolated_pop(data = fore_pop_total_mean, new_age = new_age, age = age, forecast_year = forecast_year)
+  mean <- pension_age_interpolated(interpolated_fore_total_mean)
+  
+  
+  fore_total_boost_list <- list()
+  interpolated_fore_total_boost_list <- list()
+  
+  for (j in 1:N) {
+    fore_total_boost_list[[j]] <- fore_boost_f[[j]]+fore_boost_m[[j]]
+    interpolated_fore_total_boost_list[[j]] <- linear_interpolated_pop(data = fore_total_boost_list[[j]] , new_age = new_age, age = age, forecast_year = forecast_year)
   }
+
   
-  #Calculate the sustainable pension age
-  first_below <- apply(OADR, 2, function(x) min(which(x < 0.23)))
-  pension.age <- substr(row.names(pop.t.1[first_below,]), start = 1, stop = 2)
-  final.result <- cbind(colnames(pop.t.1), pension.age)
-  colnames(final.result) <- c("year", "pension.age")
-  ######################################## 
-  #Calculate the sustainable lower bound pension age
-  pop.t.l <- pop_f.l+pop_m.l
-  pop.t.1l <- pop.t.l[c(16:101),c(102:ncol(pop.t.l))]
-  n2 <- nrow(pop.t.1l)-1
-  m2 <- ncol(pop.t.1l)
-  OADR.l <- matrix(data = 0,nrow = n2,ncol = m2,dimnames = list(rownames(pop.t.1l[-1,]),colnames(pop.t.1l)))
   
-  for (j in 1:m2) {
-    for (i in 1:n2) {
-      OADR.l[i,j] <- sum(pop.t.1l[c((i+1):(n2+1)),j])/sum(pop.t.1l[c(1:i),j])
-    }
+  
+  pension_age_upper <- pension_age_lower <- pension_age_median <- vector("numeric", N)
+  for(i in 1:N) 
+  {
+    pension_age_upper[i] <- pension_age_bound_interpolated(data = interpolated_fore_total_boost_list[[i]], alpha = 1-alpha/2)
+    # below is the median
+    pension_age_median[i] <- pension_age_bound_interpolated(data = interpolated_fore_total_boost_list[[i]], alpha = 0.5)
+    pension_age_lower[i] <- pension_age_bound_interpolated(data = interpolated_fore_total_boost_list[[i]], alpha = alpha/2)
   }
+  median <- cbind(forecast_year, pension_age_median)
+  lower <- cbind(forecast_year, pension_age_lower)
+  upper <- cbind(forecast_year, pension_age_upper)
   
-  f.l <- apply(OADR.l, 2, function(x) min(which(x < 0.23)))
-  p.l <- substr(row.names(pop.t.1l[f.l,]), start = 1, stop = 2)
-  final.result.l <- cbind(colnames(pop.t.1l), p.l)
-  colnames(final.result.l) <- c("year", "pension.age")
-  ############################################### 
-  #Calculate the sustainable upper bound pension age
-  pop.t.u <- pop_f.u+pop_m.u
-  pop.t.1u <- pop.t.u[c(16:101),c(102:ncol(pop.t.u))]
-  n2 <- nrow(pop.t.1u)-1
-  m2 <- ncol(pop.t.1u)
-  OADR.u <- matrix(data = 0,nrow = n2,ncol = m2,dimnames = list(rownames(pop.t.1u[-1,]),colnames(pop.t.1u)))
-  
-  for (j in 1:m2) {
-    for (i in 1:n2) {
-      OADR.u[i,j] <- sum(pop.t.1u[c((i+1):(n2+1)),j])/sum(pop.t.1u[c(1:i),j])
-    }
-  }
-  
-  f.u <- apply(OADR.u, 2, function(x) min(which(x < 0.23)))
-  p.u <- substr(row.names(pop.t.1u[f.u,]), start = 1, stop = 2)
-  final.result.u <- cbind(colnames(pop.t.1u), p.u)
-  colnames(final.result.u) <- c("year", "pension.age")
   ###########################################
   
-  
-  result_list <- list(pop_m = pop_m, pop_f = pop_f, orginal_f = orginal_f, orginal_m = orginal_m, CCR_m = CCR_m, CCR_f = CCR_f, pop_f.u = pop_f.u, pop_m.u = pop_m.u, pop_f.l = pop_f.l, pop_m.l = pop_m.l, final.result = final.result, final.result.l = final.result.l, final.result.u = final.result.u)
+  result_list <- list(fore_pop_m = pop_m, 
+                      fore_pop_f = pop_f, 
+                      original_f = original_f, 
+                      original_m = original_m, 
+                      CCR_m = CCR_m, 
+                      CCR_f = CCR_f, 
+                      pop_f.u = pop_f.u, 
+                      pop_m.u = pop_m.u, 
+                      pop_f.l = pop_f.l, 
+                      pop_m.l = pop_m.l, 
+                      mean = mean$final_result, 
+                      median = median,
+                      upper = upper, 
+                      lower = lower)
   
   return(result_list)
 }
 
-#Final results
-pop.30 <- population.fore(pop_f = new_f,pop_m = new_m,method = method,N = 30,f.ratio = f.ratio,m.ratio = m.ratio,level = 0.95)
+
+pop_30_au_interpolated <- list()
+for (i in 1:8) {
+  pop_30_au_interpolated[[i]] <- population_fore_interpolated(pop_f = female_matrices[[i]], pop_m = male_matrices[[i]], method = "classical", N = 30, f.ratio = f.ratio, m.ratio = m.ratio, level = 0.95, year = year, age = age)
+  print(i); rm(i)
+}
+
+
+names(pop_30_au_interpolated) <- paste(valid_States, 30, sep = "_")
+
+
+pop_30_total_au_interpolated <- population_fore_interpolated(pop_f = au_pop_f, pop_m = au_pop_m, method = "classical", N = 30, f.ratio = f.ratio, m.ratio = m.ratio, level = 0.95, year = year, age = age)
